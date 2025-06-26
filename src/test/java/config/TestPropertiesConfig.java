720f752b5bc7189f82c252219917a2dc5c3c3f97
StepanidaKirillina1 <steshakirillina@yandex.ru>
2025-06-26 14:17:57 +0900

    update yml and property files

--- src/test/java/config/TestPropertiesConfig.java
+++ src/test/java/config/TestPropertiesConfig.java
@@ -2,6 +2,11 @@ package config;
 
 import org.aeonbits.owner.Config;
 
+@Config.Sources({
+        "classpath:${env}.properties",
+        "classpath:default.properties"
+})
+
 public interface TestPropertiesConfig extends Config {
     @Key("authorization")
     String getAuthorizationValue();
