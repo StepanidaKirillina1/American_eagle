446a5f88ba863822dd06fc76c44f2f2366a49447
StepanidaKirillina1 <steshakirillina@yandex.ru>
2025-07-18 12:01:42 +0900

    refactor Token enum

--- src/test/java/utils/Token.java
+++ src/test/java/utils/Token.java
@@ -3,6 +3,7 @@ package utils;
 import controllers.UserController;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 import org.hamcrest.Matchers;
 
@@ -16,20 +17,16 @@ public enum Token {
     GUEST {
         @Override
         public String getToken() {
-            return buildTokenRequest()
-                    .formParam("grant_type", "client_credentials")
-                    .header("authorization", PROPERTIES_CONFIG.getAuthorizationValue())
-                    .header("Cookie", PROPERTIES_CONFIG.getCookieValue())
-                    .when()
-                    .post("auth/oauth/" + "v5/token")
-                    .then().log().body()
-                    .statusCode(200)
-                    .body("token_type", Matchers.equalTo("Bearer"))
-                    .body("scope", Matchers.equalTo("guest"))
-                    .extract()
-                    .body()
-                    .jsonPath()
-                    .get("access_token");
+            return extractToken(
+                    buildTokenRequest()
+                            .formParam("grant_type", "client_credentials")
+                            .header("Cookie", PROPERTIES_CONFIG.getCookieValue())
+                            .when()
+                            .post("auth/oauth/" + "v5/token")
+                            .then()
+                            .body("token_type", Matchers.equalTo("Bearer"))
+                            .body("scope", Matchers.equalTo("guest"))
+            );
         }
     },
 
@@ -46,28 +43,23 @@ public enum Token {
             String bm_sv = response.getCookie("bm_sv");
             String ak_bmsc = response.getCookie("ak_bmsc");
 
-            return buildTokenRequest()
-                    .formParam("grant_type", "password")
-                    .formParam("username", PROPERTIES_CONFIG.getLogin())
-                    .formParam("password", PROPERTIES_CONFIG.getPassword())
-                    .header("aelang", "en_US")
-                    .header("aesite", "AEO_US")
-                    .header("aecountry", "US")
-                    .header("Authorization", PROPERTIES_CONFIG.getAuthorizationValue())
-                    .header("Cookie", "TLTUID=" + TLTUID + "; " + "akaalb_PROD_ALB_API=" + akaalb_PROD_ALB_API + "; " +"ak_bmsc=" + ak_bmsc + "; " + "bm_sz=" + bm_sz + "; " + "_abck=" + abckCookie + "; " + "bm_sv=" + bm_sv)
-                    .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36")
-                    //.header("x-access-token", getGuestToken())
-                    .header("x-access-token", Token.GUEST.getToken())
-                    .header("Origin", "https://www.ae.com")
-                    .when()
-                    .post("auth/oauth/" + "v4/token")
-                    .then().log().body()
-                    .statusCode(200)
-                    .body("token_type", Matchers.equalTo("password"))
-                    .extract()
-                    .body()
-                    .jsonPath()
-                    .get("access_token");
+            return extractToken(
+                    buildTokenRequest()
+                            .formParam("grant_type", "password")
+                            .formParam("username", PROPERTIES_CONFIG.getLogin())
+                            .formParam("password", PROPERTIES_CONFIG.getPassword())
+                            .header("aelang", "en_US")
+                            .header("aesite", "AEO_US")
+                            .header("aecountry", "US")
+                            .header("Cookie", "TLTUID=" + TLTUID + "; " + "akaalb_PROD_ALB_API=" + akaalb_PROD_ALB_API + "; " +"ak_bmsc=" + ak_bmsc + "; " + "bm_sz=" + bm_sz + "; " + "_abck=" + abckCookie + "; " + "bm_sv=" + bm_sv)
+                            .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36")
+                            .header("x-access-token", Token.GUEST.getToken())
+                            .header("Origin", "https://www.ae.com")
+                            .when()
+                            .post("auth/oauth/" + "v4/token")
+                            .then().log().body()
+                            .body("token_type", Matchers.equalTo("password"))
+            );
         }
     };
 
@@ -78,5 +70,14 @@ public enum Token {
                 .contentType(ContentType.URLENC)
                 .baseUri(API_BASE_URL)
                 .header("authorization", PROPERTIES_CONFIG.getAuthorizationValue());
+    }
+
+    protected String extractToken(ValidatableResponse response) {
+        return response
+                .statusCode(200)
+                .extract()
+                .body()
+                .jsonPath()
+                .get("access_token");
     }
 }
